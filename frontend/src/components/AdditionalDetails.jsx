3763e1155a31865800c127e176fe98ba2bc2bef2
VipinKankal <154401636+VipinKankal@users.noreply.github.com>
2025-01-03 21:50:17 +0530

    no code just update

--- frontend/src/components/AdditionalDetails.jsx
+++ frontend/src/components/AdditionalDetails.jsx
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { useNavigate } from 'react-router-dom';
 import './Customercss.css';
 
+
 export default function AdditionalDetails() {
 
   const navigate = useNavigate();
